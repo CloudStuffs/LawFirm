d2394bab170425a1704a7516863e791fc27aad8c
Faizan Ayubi <indianayubi@gmail.com>
2016-01-09 15:36:55 +0530

    Design Started

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,3 +1,74 @@
-$(document).ready(function() {
-	//js
+//Zoom fix
+jQuery(function() {
+    // IPad/IPhone
+    var viewportmeta = document.querySelector && document.querySelector('meta[name="viewport"]'),
+        ua = navigator.userAgent,
+
+        gestureStart = function() {
+            viewportmeta.content = "width=device-width, minimum-scale=0.25, maximum-scale=1.6";
+        },
+
+        scaleFix = function() {
+            if (viewportmeta && /iPhone|iPad/.test(ua) && !/Opera Mini/.test(ua)) {
+                viewportmeta.content = "width=device-width, minimum-scale=1.0, maximum-scale=1.0";
+                document.addEventListener("gesturestart", gestureStart, false);
+            }
+        };
+    scaleFix();
+});
+
+jQuery(function() {
+    jQuery('.sf-menu').mobileMenu();
+});
+
+// ---------------------------------------------------------
+// Back to Top
+// ---------------------------------------------------------
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$('#back-top').fadeIn();
+		} else {
+			$('#back-top').fadeOut();
+		}
+	});
+	$('#back-top a').click(function () {
+		$('body,html').stop(false, false).animate({
+			scrollTop: 0
+		}, 800);
+		return false;
+	});
+// ---------------------------------------------------------
+// Add accordion active class
+// ---------------------------------------------------------
+	$('.accordion').on('show', function (e) {
+         $(e.target).prev('.accordion-heading').find('.accordion-toggle').addClass('active');
+    });
+    $('.accordion').on('hide', function (e) {
+        $(this).find('.accordion-toggle').not($(e.target)).removeClass('active');
+    });
+// ---------------------------------------------------------
+// Isotope Init
+// ---------------------------------------------------------
+	//$("#portfolio-grid").css({"visibility" : "visible"});
+// ---------------------------------------------------------
+// Menu Android
+// ---------------------------------------------------------
+	if(window.orientation!=undefined){
+		var regM = /ipod|ipad|iphone/gi,
+			result = navigator.userAgent.match(regM)
+		if(!result) {
+			$('.sf-menu li').each(function(){
+				if($(">ul", this)[0]){
+					$(">a", this).toggle(
+						function(){
+							return false;
+						},
+						function(){
+							window.location.href = $(this).attr("href");
+						}
+					);
+				} 
+			})
+		}
+	}
 });
